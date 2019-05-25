ab87e0314761ca37a413172ccf4b494930797104
Tianyijian <2624132357@qq.com>
2019-05-26 02:10:13 +0800

    问题分类：提取关键词测试

--- const.py
+++ const.py
@@ -11,6 +11,8 @@ train_data = 'data/train.json'
 stop_words = 'question_classification/stopwords.txt'
 qc_train_data = 'question_classification/train_questions.txt'
 qc_test_data = 'question_classification/test_questions.txt'
+
+# 中间数据
 qc_train_seg = 'question_classification/train_questions_seg.txt'
 qc_test_seg = 'question_classification/test_questions_seg.txt'
 qc_train_seg_jie_ba = 'question_classification/train_questions_seg_jie_ba.txt'
@@ -21,6 +23,13 @@ qc_train_pos = 'question_classification/train_questions_pos.txt'
 qc_test_pos = 'question_classification/test_questions_pos.txt'
 qc_train_ner = 'question_classification/train_questions_ner.txt'
 qc_test_ner = 'question_classification/test_questions_ner.txt'
+qc_train_dr = 'question_classification/train_questions_dr.txt'
+qc_test_dr = 'question_classification/test_questions_dr.txt'
+qc_train_rough_res = 'question_classification/train_rough_res.txt'
+qc_test_rough_res = 'question_classification/test_rough_res.txt'
+qc_train_fine_res = 'question_classification/train_fine_res.txt'
+qc_test_fine_res = 'question_classification/test_fine_res.txt'
+
 
 # 生成数据
 passages_seg = 'result/passages_seg.json'
--- question_classification.py
+++ question_classification.py
@@ -98,6 +98,99 @@ def ltp_ner_data():
     recognizer.release()  # 释放模型
 
 
+def ltp_parser_data():
+    """LTP 依存句法分析提取关键词"""
+    LTP_DATA_DIR = 'D:\BaiduNetdiskDownload\ltp_data_v3.4.0'  # ltp模型目录的路径
+    par_model_path = os.path.join(LTP_DATA_DIR, 'parser.model')  # 依存句法分析模型路径，模型名称为`parser.model`
+
+    from pyltp import Parser
+    parser = Parser()  # 初始化实例
+    parser.load(par_model_path)  # 加载模型
+
+    file = [(const.qc_train_pos, const.qc_train_dr), (const.qc_test_pos, const.qc_test_dr)]
+    for k in range(2):
+        result = []
+        with open(file[k][0], 'r', encoding='utf-8') as f:
+            for line in f.readlines():
+                attr = line.strip().split('\t')
+                words_pos = attr[1].split(" ")
+                words = [word.split('/_')[0] for word in words_pos]
+                # print(words)
+                postags = [word.split('/_')[1] for word in words_pos]
+
+                arcs = parser.parse(words, postags)  # 句法分析
+                # 提取核心词及其指向词
+                res = []
+                for i in range(len(arcs)):
+                    if arcs[i].head == 0:
+                        for j in range(len(arcs)):
+                            if (arcs[j].head == i + 1 or arcs[j].head == 0 or arcs[j].relation == 'ADV') and arcs[j].relation != 'WP':
+                            # if (arcs[j].head == i + 1 or arcs[j].head == 0) and arcs[j].relation != 'WP':
+                                res.append(j)
+                # print(res)
+                # 广度优先选择ATT
+                for j in res:
+                    for i in range(len(arcs)):
+                        if arcs[i].head == j+1 and arcs[i].relation == 'ATT':
+                            res.append(i)
+                res = list(set(res))
+                res.sort()
+                result.append("{}\t{}\n".format(attr[0], ' '.join([words[i] for i in res])))
+        with open(file[k][1], 'w', encoding='utf-8') as f:
+            f.writelines(result)
+    parser.release()  # 释放模型
+
+
+def ltp_parser_data_test():
+    LTP_DATA_DIR = 'D:\BaiduNetdiskDownload\ltp_data_v3.4.0'  # ltp模型目录的路径
+
+    cws_model_path = os.path.join(LTP_DATA_DIR, 'cws.model')  # 分词模型路径，模型名称为`cws.model`
+    from pyltp import Segmentor
+    segmentor = Segmentor()  # 初始化实例
+    segmentor.load(cws_model_path)  # 加载模型
+
+    pos_model_path = os.path.join(LTP_DATA_DIR, 'pos.model')  # 词性标注模型路径，模型名称为`pos.model`
+
+    from pyltp import Postagger
+    postagger = Postagger()  # 初始化实例
+    postagger.load(pos_model_path)  # 加载模型
+
+    par_model_path = os.path.join(LTP_DATA_DIR, 'parser.model')  # 依存句法分析模型路径，模型名称为`parser.model`
+    from pyltp import Parser
+    parser = Parser()  # 初始化实例
+    parser.load(par_model_path)  # 加载模型
+
+    sents = ['API的全称是什么？', '中国是世界上老年人口最多的国家吗', '艾滋病是如何传染的', '1999年NBA总冠军是哪支球队', 'Flash是哪个公司的产品']
+    for sent in sents:
+        print(sent)
+        words = segmentor.segment(sent)
+        print(list(words))
+        postags = postagger.postag(words)
+        print(list(postags))
+        arcs = parser.parse(words, postags)  # 句法分析
+        print("\t".join("%d:%s" % (arc.head, arc.relation) for arc in arcs))
+        res = []
+        for i in range(len(arcs)):
+            if arcs[i].head == 0:
+                for j in range(len(arcs)):
+                    # if (arcs[j].head == i + 1 or arcs[j].head == 0 or arcs[j].relation == 'ADV' ) and arcs[j].relation != 'WP':
+                    if (arcs[j].head == i + 1 or arcs[j].head == 0  ) and arcs[j].relation != 'WP':
+                        res.append(j)
+        print(res)
+        for j in res:
+            for i in range(len(arcs)):
+                if arcs[i].head == j+1 and arcs[i].relation == 'ATT':
+                    res.append(i)
+        res = list(set(res))
+        res.sort()
+        print(res)
+        print(' '.join([words[i] for i in res]) + '\n')
+
+    segmentor.release()  # 释放模型
+    postagger.release()  # 释放模型
+    parser.release()  # 释放模型
+
+
 def ltp_remove_stop_words():
     """去除停用词"""
     with open(const.stop_words, 'r', encoding='utf-8') as f:
@@ -137,7 +230,7 @@ def load_data(size='fine'):
     y_train = []
     x_test = []
     y_test = []
-    with open(const.qc_train_seg_jie_ba, 'r', encoding='utf-8') as f:
+    with open(const.qc_train_seg, 'r', encoding='utf-8') as f:
         for line in f.readlines():
             attr = line.strip().split('\t')
             x_train.append(attr[1])
@@ -145,7 +238,7 @@ def load_data(size='fine'):
                 y_train.append(attr[0].split('_')[0])
             else:
                 y_train.append(attr[0])
-    with open(const.qc_test_seg_jie_ba, 'r', encoding='utf-8') as f:
+    with open(const.qc_test_seg, 'r', encoding='utf-8') as f:
         for line in f.readlines():
             attr = line.strip().split('\t')
             x_test.append(attr[1])
@@ -217,6 +310,10 @@ def train_svm_fine():
     # clf = SVC(decision_function_shape='ovo')
     clf = SVC(C=100.0, gamma=0.01)
     clf.fit(train_data, y_train)
+    test_res = clf.predict(test_data)
+    # 将预测结果写入文件
+    with open(const.qc_test_fine_res, 'w', encoding='utf-8') as f:
+        f.writelines(["{}\t{}\t{}\n".format(y_test[i], test_res[i], x_test[i]) for i in range(len(x_test))])
     print(clf.score(test_data, y_test))
 
 
@@ -232,7 +329,10 @@ def train_svm_rough():
     # clf = SVC(C=100.0, kernel='linear')
     clf = SVC(C=100.0, gamma=0.1)
     clf.fit(train_data, y_train)
-    # print(clf.predict(test_data))
+    test_res = clf.predict(test_data)
+    # 将预测结果写入文件
+    with open(const.qc_test_rough_res, 'w', encoding='utf-8') as f:
+        f.writelines(["{}\t{}\t{}\n".format(y_test[i], test_res[i], x_test[i]) for i in range(len(x_test))])
     print(clf.score(test_data, y_test))
 
 
@@ -254,7 +354,7 @@ def choose_svm():
         #     'C': [1, 10, 50, 100, 500, 1000],
         #     'kernel': ['linear']
         # }
-        ]
+    ]
     x_train, y_train, x_test, y_test = load_data(size='rough')
     # x_train, y_train, x_test, y_test = load_data()
     tv = TfidfVectorizer(token_pattern=r"(?u)\b\w+\b")
@@ -279,6 +379,8 @@ if __name__ == '__main__':
     # ltp_pos_data()
     # ltp_ner_data()
     # ltp_remove_stop_words()
+    ltp_parser_data()
+    # ltp_parser_data_test()
     # jie_ba_seg()
     # train_nb()
     # train_lr()
